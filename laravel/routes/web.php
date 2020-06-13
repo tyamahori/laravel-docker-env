a9105edf10c28ab40b34bdaa2dab5eb3a1fee954
tyamahori <tyamahori@gmail.com>
2020-06-14 00:55:30 +0900

    記述修正

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    log::info('test', [
-        'message' => 'hige'
-    ]);
+    logger('test', ['message' => 'sample']);
     return view('welcome');
 });
