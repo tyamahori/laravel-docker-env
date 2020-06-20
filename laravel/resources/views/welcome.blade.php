797b1288bfcbd2cc0fc515b843ce54e9ac57535f
tyamahori <tyamahori@gmail.com>
2020-06-20 17:59:46 +0900

    debug

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -2,13 +2,12 @@
 {{ now() }}
 {{ request()->ip() }}
 {{ logger(request()->ip()) }}
-{{ logger('hoegeg') }}
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel{{ now() }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
