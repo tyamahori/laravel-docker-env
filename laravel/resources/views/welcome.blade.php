5f322afea060cf18423baf1374bceb5277566c7f
tyamahori <tyamahori@gmail.com>
2020-06-20 16:11:46 +0900

    テストコミット

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
 {{ now() }}
 {{ request()->ip() }}
 {{ logger(request()->ip()) }}
+{{ logger('hoegeg') }}
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
