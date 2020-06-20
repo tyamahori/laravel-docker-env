1146fd23425351e2a9e077bc614bbd0ad20236d7
tyamahori <tyamahori@gmail.com>
2020-06-20 15:58:00 +0900

    テストコミット

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 {{ now() }}
+{{ request()->ip() }}
+{{ logger(request()->ip()) }}
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
